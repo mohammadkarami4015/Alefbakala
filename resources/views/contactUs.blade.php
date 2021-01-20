6efcbfc87267087355a2233e44a89395dfec2a95
muhammad <mohammadkarami0815@gmail.com>
2021-01-20 17:32:41 +0330

    fix footer

---
+++ resources/views/contactUs.blade.php
@@ -0,0 +1,50 @@
+@extends('main')
+@section('title')
+    alefbakala
+@stop
+@section('navbar')
+    @include('navbar')
+@stop
+@section('content')
+    <div id="container">
+        <div class="container">
+            <!-- Breadcrumb Start-->
+            <ul class="breadcrumb">
+                <li><a href="{{route('home')}}"><i class="fa fa-home"></i></a></li>
+
+            </ul>
+            <!-- Breadcrumb End-->
+            <div class="row">
+                <!--Middle Part Start-->
+                <div id="content" class="col-sm-9">
+                    <h1 class="title">تماس با ما</h1>
+                    <h3 class="subtitle">محل ما</h3>
+                    <div class="row">
+                        <div class="col-sm-3"><img src="image/product/store_location-275x180.jpg" alt="قالب مارکت شاپ" title="قالب مارکت شاپ" class="img-thumbnail"></div>
+                        <div class="col-sm-3"><strong>آدرس شرکت</strong><br>
+                            <address>
+                                 شهران،<br>
+                                   بالاتر از فلکه دوم،<br>
+                                شاختمان محمد،<br>
+                                 </address>
+                        </div>
+                        <div class="col-sm-3"><strong>شماره تماس</strong><br>
+                            09366798946 <br>
+                            <br>
+                            <strong>تلفن دفتر</strong><br>
+                            021 44307789 </div>
+                        <div class="col-sm-3"> <strong>تحویل کالا</strong><br>
+                            شنبه تا جمعه از ساعت 7 تا 22<br>
+                            <br>
+                            <strong>پشتیبانی تلفنی</strong><br>
+                            هرگونه سوالی در خصوص انتخاب محصول و ثبت سفارش  </div>
+
+                        <strong>تماس با پشتیبانی</strong><br>
+                            02144307789  </div>
+                    </div>
+                </div>
+                <!--Middle Part End -->
+            </div>
+        </div>
+    </div>
+@stop
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,42 @@
+<footer id="footer">
+    <div class="fpart-first">
+        <div class="container">
+            <div class="row">
+                <div class="contact col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <h5>درباره الفبا کالا</h5>
+                    <p>الفباکالا از معتبرترین و به زورترین فروشگاه های اینترنتی کشور با بهره گیری از تیمی پرتلاش و خلاق، در راستای خدمت رسانی و تامین نیاز اقشار جامعه، با هر رده سنی و با هدف فراهم سازی بستری امن و متنوع جهت یک خرید راحت و مطمئن و باکیفیت می باشد.این مجموعه به طور مرتب شاهد ایجاد خدمات بروز،در راستای نیاز مشتریان عزیز می باشد و همواره خواست و نیاز شما اولویت برنامه های ما بوده و خواهد بود و در این راه از نظرات و پیشنهادات و انتقادات شما با کمال افتخار بهره خواهیم برد.</p>
+                    <img alt="" src="image/logo-small.png">
+                </div>
+
+                <div style="float: left" class="column col-lg-2 col-md-2 col-sm-3 col-xs-12">
+                    <h5>حساب من</h5>
+                    <ul>
+                        <li><a href="#">حساب کاربری</a></li>
+                        <li><a href="#">تاریخچه سفارشات</a></li>
+                        <li><a href="#">لیست علاقه مندی</a></li>
+                        <li><a href="#">خبرنامه</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fpart-second">
+        <div class="container">
+            <div id="powered" class="clearfix">
+                <div class="powered_text pull-left flip">
+                    <p>کپی رایت © 2016 فروشگاه شما | پارسی سازی و ویرایش توسط <a href="http://www.20script.ir" target="_blank">بیست اسکریپت</a></p>
+                </div>
+                <div class="social pull-right flip"> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/socialicons/facebook.png" alt="Facebook" title="Facebook"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/socialicons/twitter.png" alt="Twitter" title="Twitter"> </a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/socialicons/google_plus.png" alt="Google+" title="Google+"> </a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/socialicons/pinterest.png" alt="Pinterest" title="Pinterest"> </a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/socialicons/rss.png" alt="RSS" title="RSS"> </a> </div>
+            </div>
+            <div class="bottom-row">
+                <div class="custom-text text-center">
+                    <p>این یک بلاک مدیریت محتواست. شما میتوانید هر نوع محتوای html نوشتاری یا تصویری را در آن قرار دهید.<br> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                </div>
+                <div class="payments_types"> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_paypal.png" alt="paypal" title="PayPal"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_american.png" alt="american-express" title="American Express"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_2checkout.png" alt="2checkout" title="2checkout"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_maestro.png" alt="maestro" title="Maestro"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_discover.png" alt="discover" title="Discover"></a> <a href="#" target="_blank"> <img data-toggle="tooltip" src="image/payment/payment_mastercard.png" alt="mastercard" title="MasterCard"></a> </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+<!--Footer End-->
+<!-- Twitter Side Block Start -->
